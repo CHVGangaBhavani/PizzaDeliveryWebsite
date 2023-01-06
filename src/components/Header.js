55b048681fa1315c4971743e1bcad46040f3073d
CHVGangaBhavani <119619160+CHVGangaBhavani@users.noreply.github.com>
2023-01-06 11:38:20 +0000

    PizzaDeliverywebsite

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Navbar from "./Navbar";
+const Header = () => {
+  return (
+    <div className="banner">
+      <Navbar />
+      <div className="banner__content">
+        <div className="container">
+          <div className="banner__text">
+            <h3>Pizza Devlivery</h3>
+            <h1>MAESTRO PIZINNI</h1>
+            <p>
+            money can’t buy happiness, but it can buy pizza and that’s almost the same thing
+            </p>
+            <div className="banner__btn">
+              <a href="" className="btn btn-smart">
+                DEVLERY NOW
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
